933ce1e591d374556fb3c00268657c5a3d7d9b0e
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-09 09:47:44 +0530

    Add power calculation functionality in q1.py

---
+++ q1.py
@@ -0,0 +1,9 @@
+Power = lambda no:(no**2)
+
+def main():
+    no = int(input("Enter the number you want:"))
+    ret = Power(no)
+    print("The power is :",ret)
+
+if __name__ == "__main__":
+    main()
