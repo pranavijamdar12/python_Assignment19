490e8c8bd336ecaee90891d765cce74f5fd9b0fc
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-09 09:54:26 +0530

    Implement multiplication function in q2.py

---
+++ q2.py
@@ -0,0 +1,10 @@
+Mul = lambda no1 , no2:(no1*no2)
+
+def main():
+    no1 = int(input("Enter the first number you want:"))
+    no2 = int(input("Enter the second nuber you want:"))
+    ret = Mul(no1,no2)
+    print("The result is:",ret)
+
+if __name__ == "__main__":
+    main()
